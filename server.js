660a752b3863cd10c4891992ef1473c21487c7a4
Stephen Sugden <me@stephensugden.com>
2015-08-15 11:08:17 +0200

    stringify primitives

--- server.js
+++ server.js
@@ -35,18 +35,28 @@ app.request.value('subscriptionOffset', (request) => {
 })
 
 app.routes({
-  'GET /*':    (state, path)             => state.get(path),
   'PUT /*':    (state, path, parsedBody) => state.apply([{ op: 'add', path, value: parsedBody }]),
   'DELETE /*': (state, path)             => state.apply([{ op: 'remove', path }]),
   'PATCH /*':  (state, path, patch)      => state.apply(patch)
 })
 
-app.route('SUBSCRIBE /*', (state, path, subscriptionOffset) => {
+app.route('GET /*', (state, path) => {
+  const value = state.get(path)
+  // Return non-null objects directly
+  if (value && typeof value == 'object') {
+    return value
+  }
+  // JSON stringify primitives
   return {
-    headers: { 'Content-Type': 'application/vnd.patch_stream+json' },
-    body: state.changes(path, subscriptionOffset)
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(value)
   }
 })
+
+app.route('SUBSCRIBE /*', (state, path, subscriptionOffset) => ({
+  headers: { 'Content-Type': 'application/vnd.patch_stream+json' },
+  body: state.changes(path, subscriptionOffset)
+}))
 
 if (!module.parent) {
   app.listen(8080)
